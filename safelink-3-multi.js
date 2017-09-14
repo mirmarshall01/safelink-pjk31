03e9cda1cf4fece43566e112ed93408b505f433f
mirmarshall01 <31944052+mirmarshall01@users.noreply.github.com>
2017-09-14 14:00:49 +0700

    Update safelink-3-multi.js

--- safelink-3-multi.js
+++ safelink-3-multi.js
@@ -38,7 +38,7 @@ if(a_to_vk){
 
 function a_to_fa(){
   var a_to_vf=new Array();
-  protected_links=protected_links.replace("https://safelink-pjk31.blogspot.com/2017/09/","");
+  protected_links=protected_links.replace(" ","");
   a_to_vf=protected_links.split(",");
   return a_to_vf;
 }
